c383a210bc1a6c0e5798669bf062cf7707d5f766
Hack Reactor Student <student@hackreactor.com>
2017-03-29 15:28:21 -0700

    Completed queue function pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,24 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var count = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[count] = value;
+    count++;
   };
 
   someInstance.dequeue = function() {
+    var keys = _.keys(storage);
+    var minKey = Math.min.apply(Math, keys);
+    var holder = storage[minKey];
+    delete storage[minKey];
+    return holder;
   };
 
   someInstance.size = function() {
+    return _.keys(storage).length;
   };
 
   return someInstance;
