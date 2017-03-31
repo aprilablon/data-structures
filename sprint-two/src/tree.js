1833f19f5b906633d849f1769b4abd217dfc6f43
Hack Reactor Student <student@hackreactor.com>
2017-03-31 11:11:12 -0700

    Implemented tree functions

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,11 @@
 var Tree = function(value) {
   var newTree = {};
+  _.extend(newTree, treeMethods);
+
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+
+  newTree.children = [];
 
   return newTree;
 };
@@ -11,9 +13,26 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var child = Tree(value);
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+
+  var findTarget = function(tree) {
+    if (tree.value === target) {
+      return true;
+
+    } else if (tree.children.length > 0) {
+      for (var i = 0; i < tree.children.length; i++) {
+        if (findTarget(tree.children[i])) {
+          return true;
+        } 
+      }
+    }
+    return false;
+  }
+  return findTarget(this);
 };
 
 
@@ -21,3 +40,19 @@ treeMethods.contains = function(target) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+//newTree.children should be an array? 
+
+//adding a child should add a new tree
+	//value = value
+	//children = [];
+
+//contains will be the similar to family tree recursive function
+	//if value is target return
+	
+  //if children empty
+		// return false
+  //else perform search on every child
+    //children.forEach(child.contains(target))? 
+
