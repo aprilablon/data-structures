71b70d0ce09496e9063bec793af9a96864321e5b
Hack Reactor Student <student@hackreactor.com>
2017-03-31 21:23:03 -0700

    Implemented minimum requirements for hash tables

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,47 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  var tuple = [k, v];
+
+  var empty = this._storage.get(index) === undefined;
+
+  var sameKeys = (!empty) ? k === this._storage.get(index)[0][0] : false;
+
+  if (empty || sameKeys) {
+    this._storage.set(index, [tuple]);
+  
+  } else {
+    var firstItem = this._storage.get(index)[0];
+    this._storage.set(index, [firstItem, tuple]);
+  }
+
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+
+  if (bucket) {
+    console.log(bucket);
+    if (bucket[0][0] === k) {
+      return bucket[0][1];
+    } else {
+      return bucket[1][1];
+    }
+  } else {
+    return undefined;
+  }
+
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  
+
+  this._storage.set(index, undefined);
+
+
 };
 
 
@@ -22,5 +55,3 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
