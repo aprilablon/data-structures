8b5fd5233b7ea021f6dfb8fc940f29a67fff16f5
Hack Reactor Student <student@hackreactor.com>
2017-04-01 13:53:25 -0700

    Added parent property to trees

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,10 +1,10 @@
 var Tree = function(value) {
   var newTree = {};
+
   _.extend(newTree, treeMethods);
 
   newTree.value = value;
-
-
+  newTree.parent = null;
   newTree.children = [];
 
   return newTree;
@@ -14,15 +14,14 @@ var treeMethods = {};
 
 treeMethods.addChild = function(value) {
   var child = Tree(value);
+  child.parent = this;
   this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
-
   var findTarget = function(tree) {
     if (tree.value === target) {
       return true;
-
     } else if (tree.children.length > 0) {
       for (var i = 0; i < tree.children.length; i++) {
         if (findTarget(tree.children[i])) {
@@ -30,11 +29,8 @@ treeMethods.contains = function(target) {
         } 
       }
     }
-
-
     return false;
   };
-  
   return findTarget(this);
 };
 
@@ -45,19 +41,3 @@ treeMethods.contains = function(target) {
  addChild: constant
  contains: linear
  */
-
-
-//newTree.children should be an array? 
-
-//adding a child should add a new tree
-	//value = value
-	//children = [];
-
-//contains will be the similar to family tree recursive function
-	//if value is target return
-	
-  //if children empty
-		// return false
-  //else perform search on every child
-    //children.forEach(child.contains(target))? 
-
