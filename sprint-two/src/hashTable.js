970715b6bbb7eaa6c1e99739f7d9c0154e542997
Hack Reactor Student <student@hackreactor.com>
2017-04-01 16:56:16 -0700

    Adding functionality for resizing

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -14,6 +14,12 @@ HashTable.prototype.insert = function(k, v) {
 
   var sameKeys = (!empty) ? k === this._storage.get(index)[0][0] : false;
 
+
+  /////////////////////////////////////////
+  if (sameKeys) { this._storage.decrement(); }
+  /////////////////////////////////////////
+
+
   if (empty || sameKeys) {
     this._storage.set(index, [tuple]);
   
@@ -22,6 +28,28 @@ HashTable.prototype.insert = function(k, v) {
     this._storage.set(index, [firstItem, tuple]);
   }
 
+  //increment counter then check size
+  this._storage.increment();
+  var shouldResize = this._storage.verifySize();
+  
+  //check if we need to resize
+  if (shouldResize > 4) {
+    console.log('need to inc');
+    // collect everything in storage
+    // update this._limit
+    console.log(shouldResize);
+    // update this._storage
+    // iterate over collection
+      // pass into insert
+    // update counter in LimitedArray
+  }
+
+
+  //this._storage.verifySize(); // will be part of conditional statement
+  // check that the size is within bounds --> invoke verifySize
+  // if it isn't
+    // update this._limit (multiply by 2)
+    // set this._storage = LimitedArray(this._limit);
 };
 
 HashTable.prototype.retrieve = function(k) {
@@ -43,10 +71,23 @@ HashTable.prototype.retrieve = function(k) {
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   
-
   this._storage.set(index, undefined);
 
-
+  //resize function below
+  this._storage.decrement();
+  var shouldResize = this._storage.verifySize();
+  if (shouldResize) {
+    console.log('need to dec');
+    // collect everything in storage
+    // update this._limit
+    console.log(shouldResize);
+    // update this._storage
+    // iterate over collection
+      // pass into insert
+    // update counter in LimitedArray
+  }
+  //this._storage.verifySize(); // will be part of conditional statement
+  // invoke verifySize
 };
 
 
