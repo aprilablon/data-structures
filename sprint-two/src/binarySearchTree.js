b7cc8ff9dbeaadeab33a36220416b0b045eac836
Hack Reactor Student <student@hackreactor.com>
2017-04-01 10:53:01 -0700

    Implemented BST in pseudoclassical class pattern

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,32 +1,68 @@
 var BinarySearchTree = function(value) {
-	var tree = {};
-	tree.value = value;
-	tree.right = [];//higher
-	tree.left = [];//lower
-
-	tree.insert = function() {
-
-	};
-
-	tree.contains = function() {
-		
-	};
-
-	tree.depthFirstLog = function() {
-		
-	};
-
-	return tree;
+	this.value = value;
+	this.right = null;//higher
+	this.left = null;//lower
 };
 
+BinarySearchTree.prototype.insert = function(val) {
+  var tree = new BinarySearchTree(val);
+  
 
-/*Implement a binarySearchTree class with the following properties:
-A .left property, a binary search tree (BST) where all values are lower than than it the current value.
-A .right property, a BST where all values are higher than than it the current value.
-A .insert() method, which accepts a value and places in the tree in the correct position.
-A .contains() method, which accepts a value and returns a boolean reflecting whether or not the value is contained in the tree.
-A .depthFirstLog() method, which accepts a callback and executes it on every value contained in the tree.
-*/
+  if (this.value > tree.value) {
+    if (this.left === null) {
+      this.left = tree;
+    } else {
+      this.left.insert(val);
+    }
+  } else if (this.value < tree.value) {
+    if (this.right === null) {
+      this.right = tree;
+    } else {
+      this.right.insert(val);
+    }
+  }
+}
+
+BinarySearchTree.prototype.contains = function(val) {
+  var found = this.value === val;
+
+  if (found) {
+    console.log(this);
+    return true;
+  
+  } else {
+    if (val > this.value) {
+      if (this.right !== null) {
+        return this.right.contains(val);
+      
+      } else {
+        return false;
+      }
+    
+    } else if (val < this.value) {
+      if (this.left !== null) {
+        return this.left.contains(val);
+      
+      } else {
+        return false;
+      }
+    }
+  }
+
+
+}
+BinarySearchTree.prototype.depthFirstLog = function(func) {
+  func(this.value);
+  if (this.left !== null) {
+    this.left.depthFirstLog(func);
+  
+  } else {
+    if (this.right !== null) {
+      this.right.depthFirstLog(func);
+    }
+  }
+
+}
 
 
 
@@ -34,4 +70,7 @@ A .depthFirstLog() method, which accepts a callback and executes it on every val
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ insert: logarithmic, worst case linear
+ contains: same as insert
+ dfl: linear
  */
