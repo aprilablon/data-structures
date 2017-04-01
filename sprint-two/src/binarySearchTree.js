836c4e4cd1e4e7e509219c0e4f023841e346f3fd
jiaconis <john.t.iaconis@gmail.com>
2017-03-31 23:10:38 -0700

    Added BST framework

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,35 @@
 var BinarySearchTree = function(value) {
+	var tree = {};
+	tree.value = value;
+	tree.right = [];//higher
+	tree.left = [];//lower
+
+	tree.insert = function() {
+
+	};
+
+	tree.contains = function() {
+		
+	};
+
+	tree.depthFirstLog = function() {
+		
+	};
+
+	return tree;
 };
+
+
+/*Implement a binarySearchTree class with the following properties:
+A .left property, a binary search tree (BST) where all values are lower than than it the current value.
+A .right property, a BST where all values are higher than than it the current value.
+A .insert() method, which accepts a value and places in the tree in the correct position.
+A .contains() method, which accepts a value and returns a boolean reflecting whether or not the value is contained in the tree.
+A .depthFirstLog() method, which accepts a callback and executes it on every value contained in the tree.
+*/
+
+
+
 
 
 /*
